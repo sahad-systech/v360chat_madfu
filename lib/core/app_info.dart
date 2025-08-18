3f7d86b448cca4ac35c13b52b6ece83f09496dfe
sahad-systech <sahad@systech.ae>
2025-08-18 18:20:09 +0530

    adding messageid chacking

--- lib/core/app_info.dart
+++ lib/core/app_info.dart
@@ -1,6 +1,6 @@
-const baseUrl = 'https://view360webchat.madfusupport.com';
-const mediaUrl = 'https://view360.madfusupport.com';
-const appId = 'c81e728d9d4c2f636f067f89cc14862c';
-// const baseUrl = 'https://webchat.systech.ae';
-// const mediaUrl = 'https://syscare.systech.ae';
-// const appId = '67c6a1e7ce56d3d6fa748ab6d9af3fd7';
+// const baseUrl = 'https://view360webchat.madfusupport.com';
+// const mediaUrl = 'https://view360.madfusupport.com';
+// const appId = 'c81e728d9d4c2f636f067f89cc14862c';
+const baseUrl = 'https://webchat.systech.ae';
+const mediaUrl = 'https://syscare.systech.ae';
+const appId = '67c6a1e7ce56d3d6fa748ab6d9af3fd7';
